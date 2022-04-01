8d74ab67c44dabc173b58ef633ac8d4b89178a72
Chris Vaillancourt <cvaillan22@gmail.com>
2022-03-31 22:42:27 -0700

    feat: add app nav to site

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import { render } from "solid-js/web";
 
 import { PageHeading } from "./components/PageHeading";
-
-render(() => <PageHeading />, document.getElementById("root"));
+import AppNav from "./components/AppNav";
+render(
+  () => (
+    <>
+      <AppNav />
+      <PageHeading />
+    </>
+  ),
+  document.getElementById("root")
+);
