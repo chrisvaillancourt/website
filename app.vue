eedf89fed22c1f1c8a7054dea685efbd27965b75
Chris Vaillancourt <cvaillan22@gmail.com>
2022-06-16 21:58:09 -0700

    feat(app): add head meta properties

--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+useHead({
+  title: "Christopher Vaillancourt",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+  charset: "utf-8",
+  meta: [
+    {
+      name: "description",
+      content: "Christopher Vaillancourt's personal website.",
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
     <NuxtLayout>
