69ca037c611493982110e4f06228930e2290535e
Chris Vaillancourt <cvaillan22@gmail.com>
2022-06-16 21:20:07 -0700

    feat(components): add app nav

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NuxtLayout>
+      <AppNav />
       <NuxtPage />
     </NuxtLayout>
   </div>
---
+++ components/App/AppNav.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const links = new Set([
+  {
+    to: "/",
+    text: "Home",
+  },
+  {
+    to: "/posts",
+    text: "posts",
+  },
+  {
+    to: "/about",
+    text: "About",
+  },
+  {
+    to: "/contact",
+    text: "Contact",
+  },
+]);
+</script>
+
+<template>
+  <nav>
+    <NuxtLink v-for="{ to, text } in links" :key="text" :to="to">
+      {{ text }}
+    </NuxtLink>
+  </nav>
+</template>
+
+<style module></style>
