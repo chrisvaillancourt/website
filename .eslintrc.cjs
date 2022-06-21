5eb711cd50fc0fbf815b7585911e0ab69cd797ba
Chris Vaillancourt <cvaillan22@gmail.com>
2022-06-20 20:26:49 -0700

    chore(eslint): fix linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,6 @@
 // const isDev = import.meta.env.DEV;
 const isDev = true;
 
-const baseRules = {
-  'no-console': isDev ? 'off' : 'error',
-  'no-new-wrappers': 'error',
-  eqeqeq: ['error', 'smart'],
-  'no-return-await': 'error',
-  // 'no-void': ['error', {allowAsStatement: true}] // * May be helpful to uncomment later
-};
-
 module.exports = {
   root: true,
   env: {
@@ -17,42 +9,53 @@ module.exports = {
     es2022: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  plugins: ['vue', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+  ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     sourceType: 'module',
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.vue'],
   },
-  rules: baseRules,
-  overrides: [
-    {
-      files: ['**/*.ts', '**/*.vue'],
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: './tsconfig.json',
-        extraFileExtensions: ['.vue'],
+  rules: {
+    'no-console': isDev ? 'off' : 'error',
+    'no-new-wrappers': 'error',
+    eqeqeq: ['error', 'smart'],
+    'no-return-await': 'error',
+    // 'no-void': ['error', {allowAsStatement: true}] // * May be helpful to uncomment later
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/valid-v-slot': ['error', { allowModifiers: true }],
+    'vue/component-tags-order': [
+      'error',
+      {
+        order: ['script', 'template', 'style'],
       },
-      extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        'plugin:vue/vue3-recommended',
-        '@nuxtjs/eslint-config-typescript',
-        'prettier',
-      ],
-      rules: {
-        ...baseRules,
-        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-        'vue/valid-v-slot': ['error', { allowModifiers: true }],
-        'vue/component-tags-order': [
-          'error',
-          {
-            order: [['script', 'template'], 'style'],
-          },
-        ],
+    ],
+    'vue/block-lang': [
+      'error',
+      {
+        script: {
+          lang: 'ts',
+        },
       },
-    },
-    {
-      files: ['layouts/*.vue', 'pages/**/*.vue'],
-      rules: { 'vue/multi-word-component-names': 'off' },
-    },
-  ],
+    ],
+  },
+  // overrides: [
+  //   {
+  //     files: ['layouts/*.vue', 'pages/**/*.vue'],
+  //     rules: { 'vue/multi-word-component-names': 'off' },
+  //   },
+  // ],
+  ignorePatterns: ['.eslintrc.cjs'],
 };
