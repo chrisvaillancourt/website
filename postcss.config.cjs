84ff89703c59c50bc5dc538824df5a0a847c0c9e
Chris Vaillancourt <cvaillan22@gmail.com>
2023-05-08 16:01:41 -0700

    feat: add nav hamburger menu

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,14 @@
 const postcssJitProps = require('postcss-jit-props');
 const OpenProps = require('open-props');
-
+const postcssGlobalData = require('@csstools/postcss-global-data');
 module.exports = {
-  plugins: [postcssJitProps(OpenProps)],
+  plugins: [
+    postcssJitProps(OpenProps),
+    postcssGlobalData({
+      // needs to come before postcss-custom-media
+
+      files: ['node_modules/open-props/media.min.css'],
+    }),
+    require('postcss-custom-media'),
+  ],
 };
