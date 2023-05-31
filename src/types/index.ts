00c0f172298813ba9269e66def93e72904c7b2c2
Chris Vaillancourt <cvaillan22@gmail.com>
2023-05-31 12:35:17 -0700

    feat(layouts/post): render additional post metadata

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,6 @@
 import type { CollectionEntry } from 'astro:content';
 import type { blogCollectionName } from '@/content/config';
 
-interface PostFrontmatter {
-  title: string;
-  summary: string;
-  draft?: boolean;
-  added: string;
-  updated?: string;
-}
 type BlogPost = CollectionEntry<typeof blogCollectionName>;
 
-export type { PostFrontmatter, BlogPost };
+export type { BlogPost };
