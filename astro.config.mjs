4ad1bbeee98a0356500030c865b153cf95b308f1
Chris Vaillancourt <cvaillan22@gmail.com>
2023-06-01 14:00:24 -0700

    build: compress html

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ export default defineConfig({
   // introduced in v2.4
   // https://github.com/withastro/roadmap/pull/543
   scopedStyleStrategy: 'class',
-  // compressHTML: true,
+  compressHTML: true,
 });
