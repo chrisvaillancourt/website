8b4fe8fa92b6112b419ee3a48c9db48b1ff68375
Chris Vaillancourt <cvaillan22@gmail.com>
2023-06-02 16:51:36 -0700

    docs(postcss): expect ts error

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,13 @@
 const postcssJitProps = require('postcss-jit-props');
 const OpenProps = require('open-props');
 const postcssGlobalData = require('@csstools/postcss-global-data');
+
 module.exports = {
   plugins: [
     postcssJitProps(OpenProps),
+    // @ts-expect-error We use the correct syntax but the shipped types are wrong
     postcssGlobalData({
       // needs to come before postcss-custom-media
-
       files: ['node_modules/open-props/media.min.css'],
     }),
     require('postcss-custom-media'),
