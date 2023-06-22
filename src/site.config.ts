27bcabcbf91c46668fec3798e3d518325b2fe1c1
Chris Vaillancourt <cvaillan22@gmail.com>
2023-06-22 16:34:08 -0700

    feat: set site config

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,38 +1,24 @@
-interface SiteConfig {
-	author: string;
-	title: string;
-	description: string;
-	lang: string;
-	ogLocale: string;
-	themeColorLight: string;
-	themeColorDark: string;
-	date: {
-		locale: string | string[] | undefined;
-		options: Intl.DateTimeFormatOptions;
-	};
-}
-
-export const siteConfig: SiteConfig = {
+export const siteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'Chris Williams',
+	author: 'Chris Vaillancourt',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'Astro Theme Cactus',
+	title: 'Chris Vaillancourt',
 	// Meta property used as a default description meta property
-	description: 'An opinionated starter theme for Astro',
+	description: "Chris Vaillancourt's personal website",
 	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: 'en-GB',
+	lang: 'en-US',
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: 'en_GB',
+	ogLocale: 'en_US',
 	// Sets the meta data theme-color, found in src/components/BaseHead.astro L:34. Toggling the dark mode will update the meta content with either light/dark color, implementation in src/layouts/Base.astro L:41.
 	themeColorLight: '#fafafa',
 	themeColorDark: '#1d1f21',
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: 'en-GB',
+		locale: 'en-US',
 		options: {
 			day: 'numeric',
 			month: 'short',
 			year: 'numeric',
 		},
 	},
-};
+} as const;
