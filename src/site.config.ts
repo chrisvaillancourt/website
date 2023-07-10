e520694a5a9afce08a4fccc5a5ba0a52b629051b
Chris Vaillancourt <cvaillan22@gmail.com>
2023-07-10 16:09:38 -0700

    fix(theme): avoid theme fouc

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,17 +1,19 @@
+import { LIGHT_THEME_VALUE, DARK_THEME_VALUE } from '@/lib/theme';
+
 export const siteConfig = {
-	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+	// Used as both a meta property (src/components/BaseHead.astro) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: 'Chris Vaillancourt',
-	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro
 	title: 'Chris Vaillancourt',
 	// Meta property used as a default description meta property
 	description: "Chris Vaillancourt's personal website",
-	// HTML lang property, found in src/layouts/Base.astro L:18
+	// HTML lang property, found in src/layouts/Base.astro
 	lang: 'en-US',
-	// Meta property, found in src/components/BaseHead.astro L:42
+	// Meta property, found in src/components/BaseHead.astro
 	ogLocale: 'en_US',
-	// Sets the meta data theme-color, found in src/components/BaseHead.astro L:34. Toggling the dark mode will update the meta content with either light/dark color, implementation in src/layouts/Base.astro L:41.
-	themeColorLight: '#fafafa',
-	themeColorDark: '#1d1f21',
+	// Sets the meta data theme-color, found in src/components/BaseHead.astro. Toggling the dark mode will update the meta content with either light/dark color, implementation in src/layouts/Base.astro.
+	themeColorLight: LIGHT_THEME_VALUE,
+	themeColorDark: DARK_THEME_VALUE,
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: 'en-US',
