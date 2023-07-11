779e8032d9834942e9e7cbd64e6f2f1fde03a6d4
Chris Vaillancourt <cvaillan22@gmail.com>
2023-07-11 16:51:34 -0700

    feat: use daisyui theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,8 @@ import type { Config } from 'tailwindcss';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 import tailwindTypography from '@tailwindcss/typography';
 import tailwindAspectRatio from '@tailwindcss/aspect-ratio';
-import plugin from 'tailwindcss/plugin';
 import daisyui from 'daisyui';
+import { LIGHT_THEME_NAME, DARK_THEME_NAME } from './src/lib/theme';
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
@@ -22,13 +22,6 @@ export default {
 	},
 	theme: {
 		extend: {
-			colors: {
-				bgColor: 'var(--theme-bg)',
-				textColor: 'var(--theme-text)',
-				link: 'var(--theme-link)',
-				accent: 'var(--theme-accent)',
-				'accent-2': 'var(--theme-accent-2)',
-			},
 			fontFamily: {
 				// Add any custom fonts here
 				sans: [...fontFamily.sans],
@@ -37,92 +30,70 @@ export default {
 			transitionProperty: {
 				height: 'height',
 			},
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			// Remove above once tailwindcss exposes theme type
 			typography: (theme) => ({
-				cactus: {
-					css: {
-						'--tw-prose-body': 'var(--theme-text)',
-						'--tw-prose-headings': 'var(--theme-accent-2)',
-						'--tw-prose-links': 'var(--theme-text)',
-						'--tw-prose-bold': 'var(--theme-text)',
-						'--tw-prose-bullets': 'var(--theme-text)',
-						'--tw-prose-quotes': 'var(--theme-quote)',
-						'--tw-prose-code': 'var(--theme-text)',
-						'--tw-prose-hr': '0.5px dashed #666',
-						'--tw-prose-th-borders': '#666',
-					},
-				},
-				DEFAULT: {
-					css: {
-						a: {
-							'@apply cactus-link no-underline': '',
-						},
-						strong: {
-							fontWeight: '700',
-						},
-						code: {
-							border: '1px dotted #666',
-							borderRadius: '2px',
-						},
-						blockquote: {
-							borderLeftWidth: 'none',
-						},
-						hr: {
-							borderTopStyle: 'dashed',
-						},
-						thead: {
-							borderBottomWidth: 'none',
-						},
-						'thead th': {
-							fontWeight: '700',
-							borderBottom: '1px dashed #666',
-						},
-						'tbody tr': {
-							borderBottomWidth: 'none',
-						},
-						tfoot: {
-							borderTop: '1px dashed #666',
-						},
-					},
-				},
-				sm: {
-					css: {
-						code: {
-							fontSize: theme('fontSize.sm')[0],
-							fontWeight: '400',
-						},
-					},
-				},
+				// TODO look at daisyUI + typography; what do we lose by removing this?
+				// cactus: {
+				// 	css: {
+				// 		'--tw-prose-body': 'var(--theme-text)',
+				// 		'--tw-prose-headings': 'var(--theme-accent-2)',
+				// 		'--tw-prose-links': 'var(--theme-text)',
+				// 		'--tw-prose-bold': 'var(--theme-text)',
+				// 		'--tw-prose-bullets': 'var(--theme-text)',
+				// 		'--tw-prose-quotes': 'var(--theme-quote)',
+				// 		'--tw-prose-code': 'var(--theme-text)',
+				// 		'--tw-prose-hr': '0.5px dashed #666',
+				// 		'--tw-prose-th-borders': '#666',
+				// 	},
+				// },
+				// DEFAULT: {
+				// 	css: {
+				// 		a: {
+				// 			'@apply cactus-link no-underline': '',
+				// 		},
+				// 		strong: {
+				// 			fontWeight: '700',
+				// 		},
+				// 		code: {
+				// 			border: '1px dotted #666',
+				// 			borderRadius: '2px',
+				// 		},
+				// 		blockquote: {
+				// 			borderLeftWidth: 'none',
+				// 		},
+				// 		hr: {
+				// 			borderTopStyle: 'dashed',
+				// 		},
+				// 		thead: {
+				// 			borderBottomWidth: 'none',
+				// 		},
+				// 		'thead th': {
+				// 			fontWeight: '700',
+				// 			borderBottom: '1px dashed #666',
+				// 		},
+				// 		'tbody tr': {
+				// 			borderBottomWidth: 'none',
+				// 		},
+				// 		tfoot: {
+				// 			borderTop: '1px dashed #666',
+				// 		},
+				// 	},
+				// },
+				// sm: {
+				// 	css: {
+				// 		code: {
+				// 			fontSize: theme('fontSize.sm')[0],
+				// 			fontWeight: '400',
+				// 		},
+				// 	},
+				// },
 			}),
 		},
 	},
-	plugins: [
-		tailwindTypography,
-		tailwindAspectRatio,
-		daisyui,
-		plugin(function ({ addComponents }) {
-			addComponents({
-				'.cactus-link': {
-					'@apply bg-[size:100%_6px] bg-bottom bg-repeat-x': {},
-					backgroundImage:
-						'linear-gradient(transparent,transparent 5px,var(--theme-text) 5px,var(--theme-text))',
-					'&:hover': {
-						backgroundImage:
-							'linear-gradient(transparent,transparent 4px,var(--theme-link) 4px,var(--theme-link))',
-					},
-				},
-				'.title': {
-					'@apply text-2xl font-semibold text-accent-2': {},
-				},
-			});
-		}),
-	],
+	plugins: [tailwindTypography, tailwindAspectRatio, daisyui],
 	daisyui: {
 		// * see https://daisyui.com/docs/config/
-		themes: [], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-		darkTheme: 'dark', // name of one of the included themes for dark mode
+		themes: [LIGHT_THEME_NAME, DARK_THEME_NAME], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		darkTheme: DARK_THEME_NAME, // name of one of the included themes for dark mode
 		base: true, // applies background color and foreground color for root element by default
 		styled: true, // include daisyUI colors and design decisions for all components
 		utils: true, // adds responsive and modifier utility classes
