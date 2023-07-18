e8fc358385e6e01c0f792a1d65fc8d7b80c0eda3
Chris Vaillancourt <cvaillan22@gmail.com>
2023-07-18 10:20:36 -0700

    fix(theme): increase light theme accent color contrast

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,20 +1,42 @@
 // TODO consolidate config in one location / move to src/site.config.ts
-/** The name attribute of the theme meta element. */
-const METADATA_NAME = 'theme-color';
-/** The emitted event type when the user changes the theme. */
-const THEME_CHANGE_EVENT_TYPE = 'theme-change';
+// @ts-expect-error there's no type provided for the theme
+import daisyuiThemes from 'daisyui/src/theming/themes';
 
 // ! NEED TO MANUALLY SYNC THEME COLOR IN `public/manifest.webmanifest
 
 const LIGHT_THEME_NAME = 'winter';
-const LIGHT_THEME_VALUE = '#fff';
+// The default light theme accent is slightly too light.
+// darking it slightly makes it pass accessibility checks
+// created by dropping default lightness by 1 then converting from hsl to hex
+const LIGHT_THEME_ACCENT = '#bf45ab';
+
+const LIGHT_THEME = Object.freeze({
+	...daisyuiThemes[`[data-theme=${LIGHT_THEME_NAME}]`],
+	accent: LIGHT_THEME_ACCENT,
+});
+const LIGHT_THEME_VALUE = LIGHT_THEME['base-100'];
 
 const DARK_THEME_NAME = 'night';
-// dark theme value based on background color
-const DARK_THEME_VALUE = '#3abff8';
+const DARK_THEME = Object.freeze({
+	...daisyuiThemes[`[data-theme=${DARK_THEME_NAME}]`],
+});
+
+const DARK_THEME_VALUE = DARK_THEME['base-100'];
+
+/** The name attribute of the theme meta element. */
+
+/** The emitted event type when the user changes the theme. */
+const THEME_CHANGE_EVENT_TYPE = 'theme-change';
+
+const THEMES = [
+	{
+		[DARK_THEME_NAME]: DARK_THEME,
+		[LIGHT_THEME_NAME]: LIGHT_THEME,
+	},
+] as const;
 
 export {
-	METADATA_NAME,
+	THEMES,
 	THEME_CHANGE_EVENT_TYPE,
 	LIGHT_THEME_NAME,
 	LIGHT_THEME_VALUE,
