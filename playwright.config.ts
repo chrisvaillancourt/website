0b3436aa96431d8c7815c8f893b7baca01ed7d5c
Chris Vaillancourt <cvaillan22@gmail.com>
2024-03-28 23:32:46 -0700

    test: add mobile viewports

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { env } from './src/lib/env';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
 export default defineConfig({
 	testDir: './e2e',
 	/* Run tests in files in parallel */
@@ -30,42 +31,27 @@ export default defineConfig({
 			name: 'chromium',
 			use: { ...devices['Desktop Chrome'] },
 		},
-
 		{
 			name: 'firefox',
 			use: { ...devices['Desktop Firefox'] },
 		},
-
 		{
 			name: 'webkit',
 			use: { ...devices['Desktop Safari'] },
 		},
-
 		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
-
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
+		{
+			name: 'Mobile Chrome',
+			use: {
+				...devices['Pixel 5'],
+			},
+		},
+		// consider adding more variety of viewport sizes
+		{
+			name: 'Mobile Safari',
+			use: {
+				...devices['iPhone 12 Mini'],
+			},
+		},
 	],
-
-	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:4321',
-	//   reuseExistingServer: !process.env.CI,
-	// },
 });
