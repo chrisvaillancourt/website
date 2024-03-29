5a90d53425d6d4ff69686a83b0959f6f956666b4
Chris Vaillancourt <cvaillan22@gmail.com>
2024-03-29 02:05:06 -0700

    test: add vitest

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import { getViteConfig } from 'astro/config';
+
+export default getViteConfig({
+	test: {
+		exclude: [
+			'**/node_modules/**',
+			'**/dist/**',
+			'**/cypress/**',
+			'**/.{idea,git,cache,output,temp}/**',
+			'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+			'e2e/**',
+		],
+		includeSource: ['src/**/*.{js,ts}'],
+	},
+});
